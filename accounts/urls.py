e884788ccf2c317e503cbd040648f9e18eff9c7e
Saif199001 <zeeshais8@gmail.com>
2026-04-09 02:24:24 +0530

    accounts api urls update

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,11 +3,11 @@ from .views import login_view, signup_view, logout_view
 from .api import signup_api, login_api, logout_api
 
 urlpatterns = [
-    path("", login_view),
-    path("login/", login_view),
-    path("signup/", signup_view),
-    path("logout/", logout_view),
-    path("api/signup/", signup_api),
-    path("api/login/", login_api),
-    path("api/logout/", logout_api)
+    # path("", login_view),
+    # path("login/", login_view),
+    # path("signup/", signup_view),
+    # path("logout/", logout_view),
+    path("signup/", signup_api),
+    path("login/", login_api),
+    path("logout/", logout_api)
 ]
