effbbf034b301fd6f62d29c34b872b80e853827c
Mohammad Saifullah <97229232+Saif199001@users.noreply.github.com>
2026-09-03 20:05:13 +0530

    Resolve tenant migration graph conflict

--- tenant/migrations/0009_alter_tenant_profile_photo.py
+++ tenant/migrations/0009_alter_tenant_profile_photo.py
@@ -1,21 +1,9 @@
 from django.db import migrations
-import cloudinary.models
 
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("tenant", "0008_rename_tenant__workspace_idx_tenant_tena_workspa_9b2ad6_idx"),
+        ("tenant", "0009_allow_null_profile_photo"),
     ]
 
-    operations = [
-        migrations.AlterField(
-            model_name="tenant",
-            name="profile_photo",
-            field=cloudinary.models.CloudinaryField(
-                "tenants_photo",
-                blank=True,
-                null=True,
-                default=None,
-            ),
-        ),
-    ]
+    operations = []
