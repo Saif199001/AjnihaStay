2f003ddff7278ed50f066cbb8cb06e93af174232
Mohammad Saifullah <97229232+Saif199001@users.noreply.github.com>
2026-09-03 19:46:16 +0530

    allow nullable tenant profile photo

---
+++ tenant/migrations/0009_allow_null_profile_photo.py
@@ -0,0 +1,22 @@
+from django.db import migrations
+import cloudinary.models
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("tenant", "0008_rename_tenant__workspace_idx_tenant_tena_workspa_9b2ad6_idx"),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name="tenant",
+            name="profile_photo",
+            field=cloudinary.models.CloudinaryField(
+                blank=True,
+                default=None,
+                max_length=255,
+                null=True,
+                verbose_name="tenants_photo",
+            ),
+        ),
+    ]
