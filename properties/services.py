a5290aa0c7f0074df77bcf0a6e721f3d066f4cfb
Mohammad Saifullah <97229232+Saif199001@users.noreply.github.com>
2026-09-03 18:07:54 +0530

    Make property creation workspace-aware

--- properties/services.py
+++ properties/services.py
@@ -1,41 +1,34 @@
 from django.core.exceptions import ValidationError
+from django.db import transaction
+
 from .models import Property, PropertyImage
 
-SUBUNIT_PROPERTY_TYPES = [
-    "pg",
-    "hostel"
-]
+SUBUNIT_PROPERTY_TYPES = ["pg", "hostel"]
 
 
-def create_property(user, data, files):
-
+def create_property(user, workspace, data, files):
     if not data.get("name"):
         raise ValidationError("Property name is required")
-
     if not data.get("property_type"):
         raise ValidationError("Property type is required")
 
-    property = Property.objects.create(
-        owner=user,
-        name=data.get("name"),
-        property_type=data.get("property_type"),
-        has_subunits=(data.get("property_type") in SUBUNIT_PROPERTY_TYPES),
-        description=data.get("description"),
-        address=data.get("address"),
-        city=data.get("city"),
-        state=data.get("state"),
-        pincode=data.get("pincode"),
-        amenities=data.get("amenities") or [],
-        thumbnail=files.get("thumbnail")
-    )
-
-    # 🔥 MULTIPLE IMAGES SUPPORT
-    images = files.getlist("images")
-
-    for img in images:
-        PropertyImage.objects.create(
-            property=property,
-            image=img
+    with transaction.atomic():
+        property_obj = Property.objects.create(
+            owner=user,
+            workspace=workspace,
+            name=data.get("name"),
+            property_type=data.get("property_type"),
+            has_subunits=(data.get("property_type") in SUBUNIT_PROPERTY_TYPES),
+            description=data.get("description"),
+            address=data.get("address"),
+            city=data.get("city"),
+            state=data.get("state"),
+            pincode=data.get("pincode"),
+            amenities=data.get("amenities") or [],
+            thumbnail=files.get("thumbnail"),
         )
 
-    return property
+        for img in files.getlist("images"):
+            PropertyImage.objects.create(property=property_obj, image=img)
+
+    return property_obj
