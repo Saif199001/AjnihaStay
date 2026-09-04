147179df275b726639b08b29d06f79f06e7b04e8
Mohammad Saifullah <97229232+Saif199001@users.noreply.github.com>
2026-09-04 16:16:47 +0530

    Add dashboard API route

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import dashboard_view
+
+from .views import DashboardAPIView, dashboard_view
 
 urlpatterns = [
     path("dashboard/", dashboard_view),
+    path("api/dashboard/", DashboardAPIView.as_view()),
 ]
