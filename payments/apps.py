0a105ea400f625c1fe73beb5f27ef148bb13b396
Mohammad Saifullah <97229232+Saif199001@users.noreply.github.com>
2026-09-09 01:53:54 +0530

    feat(payments): register PaymentRefund model

--- payments/apps.py
+++ payments/apps.py
@@ -7,4 +7,5 @@ class PaymentsConfig(AppConfig):
 
     def ready(self):
         import payments.billing_models  # noqa: F401
+        import payments.refund_models  # noqa: F401
         import payments.signals  # noqa: F401
