bcfb0f3f664d60ea303eb28083b85a18e029fd32
Mohammad Saifullah <97229232+Saif199001@users.noreply.github.com>
2026-09-11 01:14:58 +0530

    P0.10: detach superseded settlement model

--- payments/apps.py
+++ payments/apps.py
@@ -9,5 +9,4 @@ class PaymentsConfig(AppConfig):
         import payments.billing_models  # noqa: F401
         import payments.refund_models  # noqa: F401
         import payments.late_fee_models  # noqa: F401
-        import payments.settlement_models  # noqa: F401
         import payments.signals  # noqa: F401
