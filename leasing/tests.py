eb4559c6729a95717b7260424a9fb507c5f5c5e0
Mohammad Saifullah <97229232+Saif199001@users.noreply.github.com>
2026-09-12 14:02:57 +0530

    test(p1.2): verify lease status database constraint

--- leasing/tests.py
+++ leasing/tests.py
@@ -143,6 +143,12 @@ class LeaseModelTests(TestCase):
         with self.assertRaises(ValidationError):
             Lease.objects.create(**self._lease_kwargs(status="not_a_status"))
 
+    def test_lease_status_has_database_constraint(self):
+        lease = Lease(**self._lease_kwargs(status="not_a_status"))
+        with self.assertRaises(IntegrityError):
+            with transaction.atomic():
+                Lease.objects.bulk_create([lease])
+
     def test_lease_rejects_negative_financial_values(self):
         with self.assertRaises(IntegrityError):
             with transaction.atomic():
